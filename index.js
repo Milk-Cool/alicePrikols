993d4a8511cd9aa294c05b51144d370613372b1c
Milk-Cool <milan-pl@mail.ru>
2024-05-13 20:28:42 +0300

    PORT variable

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const modules = require("./modules/index.js");
 
-const app = express();
-const port = 3100;
+const { DEBUG, PORT } = process.env;
 
-const { DEBUG } = process.env;
+const app = express();
+const port = PORT ? parseInt(PORT) : 3100;
 
 app.use(express.json());
 
